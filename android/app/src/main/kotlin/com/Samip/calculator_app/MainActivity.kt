0647cad18c2eb221bc1b99389fde214738fc1e8b
Samip Subedi <samipsubedi68@gmail.com>
2024-12-11 12:39:50 +0545

    Calculator done

---
+++ android/app/src/main/kotlin/com/Samip/calculator_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.Samip.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculator_app/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CalculatorView(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
